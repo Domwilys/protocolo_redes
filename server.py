15979e4b2ed9a589b08ab08e4cd429752858bc6e
Domwilys <94638288+Domwilys@users.noreply.github.com>
2023-05-21 21:49:01 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,52 @@
+import threading
+import socket
+
+clientes = []
+
+def main():
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+
+        server.bind(('192.168.0.120', 10101))
+        server.listen(2)
+        print("Servidor conectado")
+
+    except:
+
+        print('\nNão foi possível iniciar o servidor.\n')
+        exit()
+    
+    while True:
+
+        cliente, addr = server.accept()
+        clientes.append(cliente)
+
+        thread = threading.Thread(target=confirmacao, args=[cliente])
+        thread.start()
+
+def confirmacao(cliente):
+
+    while True:
+
+        try:
+
+            msgRecv = cliente.recv(2048)
+            msg = msgRecv.decode()
+            print(msg)
+            
+            if(msg == 'y' or msg == 'Y'):
+                
+                print('Confirmação do jogador realizada')
+                
+        except:
+
+            deleteClient(cliente)
+            exit()
+
+def deleteClient(cliente):
+
+    clientes.remove(cliente)
+
+main()
